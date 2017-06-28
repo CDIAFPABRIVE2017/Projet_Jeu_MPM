2a65831d772d4683d3f90d978df041a46cf15e67
Mylia88 <mylia20@hotmail.fr>
2017-06-28 12:07:14 +0200

    test

--- Projet_Jeu_Quinto/Partie.cs
+++ Projet_Jeu_Quinto/Partie.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+<<<<<<< HEAD
 
 namespace Projet_Jeu_Quinto
 {
@@ -16,5 +17,34 @@ namespace Projet_Jeu_Quinto
         {
             InitializeComponent();
         }
+=======
+using System.Diagnostics;
+
+namespace Projet_Jeu_Quinto
+{
+    public partial class txtAffichageMot : Form
+    {
+        public txtAffichageMot()
+        {
+            InitializeComponent();
+        }
+
+        private void lblChrono_Click(object sender, EventArgs e)
+        {
+            Stopwatch chrono = new Stopwatch();
+
+            if (/*//BtnNvlPartie_Click))*/)
+                {
+                chrono.Start();
+                Thread.Sleep(10000);
+                }
+
+            if(/*BtnQuitter || mancheFini)*/)
+            {
+                chrono.Stop();
+                TimeSpan ts = Stopwatch.Elapsed;
+            }
+        }
+>>>>>>> 836e81ec15a726ec53ab74a6bbc22f2fdf132b1c
     }
 }
