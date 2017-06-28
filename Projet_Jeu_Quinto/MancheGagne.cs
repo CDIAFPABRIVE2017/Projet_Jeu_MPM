836e81ec15a726ec53ab74a6bbc22f2fdf132b1c
Priss01 <precilliaalvarez@yahoo.fr>
2017-06-28 11:44:16 +0200

    Frm fini

---
+++ Projet_Jeu_Quinto/MancheGagne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet_Jeu_Quinto
+{
+    public partial class MancheGagne : Form
+    {
+        public MancheGagne()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Projet_Jeu_Quinto/Partie.cs
+++ Projet_Jeu_Quinto/Partie.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Projet_Jeu_Quinto
 {
-    public partial class Partie : Form
+    public partial class txtAffichageMot : Form
     {
-        public Partie()
+        public txtAffichageMot()
         {
             InitializeComponent();
+        }
+
+        private void lblChrono_Click(object sender, EventArgs e)
+        {
+            Stopwatch chrono = new Stopwatch();
+
+            if (/*//BtnNvlPartie_Click))*/)
+                {
+                chrono.Start();
+                Thread.Sleep(10000);
+                }
+
+            if(/*BtnQuitter || mancheFini)*/)
+            {
+                chrono.Stop();
+                TimeSpan ts = Stopwatch.Elapsed;
+            }
         }
     }
 }
